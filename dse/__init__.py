d542cb0c5b3418203cd6251a28c7d36558c45c78
Jim Witschey <jim.witschey@gmail.com>
2017-02-13 15:14:08 -0500

    update docs ref for 2.0.0b3

--- dse/__init__.py
+++ dse/__init__.py
@@ -16,7 +16,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger('dse').addHandler(NullHandler())
 
-__version_info__ = (2, 0, '0b3')
+__version_info__ = (2, 0, '0b3', 'post0')
 __version__ = '.'.join(map(str, __version_info__))
 
 
