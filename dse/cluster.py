datastax/python-driver

implement and test MonotonicTimestampGenerator

cassandra/cluster.py
@@ -70,6 +70,7 @@ from cassandra.pool import (Host, _ReconnectionHandler, _HostReconnectionHandler
 from cassandra.query import (SimpleStatement, PreparedStatement, BoundStatement,
                              BatchStatement, bind_params, QueryTrace,
                              named_tuple_factory, dict_factory, tuple_factory, FETCH_SIZE_UNSET)
+from cassandra.timestamps import MonotonicTimestampGenerator
 
 
 def _is_eventlet_monkey_patched():
@@ -771,7 +772,8 @@ class Cluster(object):
                  prepare_on_all_hosts=True,
                  reprepare_on_up=True,
                  execution_profiles=None,
-                 allow_beta_protocol_version=False):
+                 allow_beta_protocol_version=False,
+                 timestamp_generator=None):
         """
         ``executor_threads`` defines the number of threads in a pool for handling asynchronous tasks such as
         extablishing connection pools or refreshing metadata.
@@ -829,6 +831,13 @@ class Cluster(object):
 
         if connection_class is not None:
             self.connection_class = connection_class
+
+        if timestamp_generator is not None:
+            if not callable(timestamp_generator):
+                raise ValueError("timestamp_generator must be callable")
+            self.timestamp_generator = timestamp_generator
+        else:
+            self.timestamp_generator = MonotonicTimestampGenerator()
 
         self.profile_manager = ProfileManager()
         self.profile_manager.profiles[EXEC_PROFILE_DEFAULT] = ExecutionProfile(self.load_balancing_policy,
@@ -1893,6 +1902,27 @@ class Session(object):
     .. versionadded:: 2.1.0
     """
 
+    timestamp_generator = None
+    """
+    When :attr:`use_client_timestamp` is set, sessions call this object and use
+    the result as the timestamp.  (Note that timestamps specified within a CQL
+    query will override this timestamp.)  By default, a new
+    :class:`~.MonotonicTimestampGenerator` is created for
+    each :class:`Cluster` instance.
+
+    Applications can set this value for custom timestamp behavior.  For
+    example, an application could share a timestamp generator across
+    :class:`Cluster` objects to guarantee that the application will use unique,
+    increasing timestamps across clusters, or set it to to ``lambda:
+    int(time.time() * 1e6)`` if losing records over clock inconsistencies is
+    acceptable for the application. Custom :attr:`timestamp_generator` s should
+    be callable, and calling them should return an integer representing seconds
+    since some point in time, typically UNIX epoch.
+
+    .. versionadded:: 3.8.0
+    """
+
+
     encoder = None
     """
     A :class:`~cassandra.encoder.Encoder` instance that will be used when
@@ -2085,7 +2115,7 @@ class Session(object):
 
         start_time = time.time()
         if self._protocol_version >= 3 and self.use_client_timestamp:
-            timestamp = int(start_time * 1e6)
+            timestamp = self.cluster.timestamp_generator()
         else:
             timestamp = None
 

tests/unit/test_timestamps.py
@@ -1,0 +1,168 @@
+# Copyright 2013-2016 DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # noqa
+
+import mock
+
+from cassandra import timestamps
+
+
+class _TimestampTestMixin(object):
+
+    @mock.patch('cassandra.timestamps.time')
+    def _call_and_check_results(self,
+                                patched_time_module,
+                                system_time_expected_stamp_pairs,
+                                timestamp_generator=None):
+        """
+        For each element in an iterable of (system_time, expected_timestamp)
+        pairs, call a :class:`cassandra.timestamps.MonotonicTimestampGenerator`
+        with system_times as the underlying time.time() result, then assert
+        that the result is expected_timestamp. Skips the check if
+        expected_timestamp is None.
+        """
+        patched_time_module.time = mock.Mock()
+        system_times, expected_timestamps = zip(*system_time_expected_stamp_pairs)
+
+        patched_time_module.time.side_effect = system_times
+        tsg = timestamp_generator or timestamps.MonotonicTimestampGenerator()
+
+        for expected in expected_timestamps:
+            actual = tsg()
+            if expected is not None:
+                self.assertEqual(actual, expected)
+
+        # assert we patched timestamps.time.time correctly
+        with self.assertRaises(StopIteration):
+            tsg()
+
+
+class TestTimestampGeneratorOutput(unittest.TestCase, _TimestampTestMixin):
+    """
+    Mock time.time and test the output of MonotonicTimestampGenerator.__call__
+    given different patterns of changing results.
+    """
+
+    def test_timestamps_during_and_after_same_system_time(self):
+        """
+        Timestamps should increase monotonically over repeated system time.
+
+        Test that MonotonicTimestampGenerator's output increases by 1 when the
+        underlying system time is the same, then returns to normal when the
+        system time increases again.
+        """
+        self._call_and_check_results(
+            system_time_expected_stamp_pairs=(
+                (15.0, 15 * 1e6),
+                (15.0, 15 * 1e6 + 1),
+                (15.0, 15 * 1e6 + 2),
+                (15.01, 15.01 * 1e6))
+        )
+
+    def test_timestamps_during_and_after_backwards_system_time(self):
+        """
+        Timestamps should increase monotonically over system time going backwards.
+
+        Test that MonotonicTimestampGenerator's output increases by 1 when the
+        underlying system time goes backward, then returns to normal when the
+        system time increases again.
+        """
+        self._call_and_check_results(
+            system_time_expected_stamp_pairs=(
+                (15.0, 15 * 1e6),
+                (13.0, 15 * 1e6 + 1),
+                (14.0, 15 * 1e6 + 2),
+                (13.5, 15 * 1e6 + 3),
+                (15.01, 15.01 * 1e6))
+        )
+
+
+class TestTimestampGeneratorLogging(unittest.TestCase, _TimestampTestMixin):
+
+    def setUp(self):
+        self.log_patcher = mock.patch('cassandra.timestamps.log')
+        self.addCleanup(self.log_patcher.stop)
+        self.patched_timestamp_log = self.log_patcher.start()
+
+    def assertLastCallArgRegex(self, call, pattern):
+        last_warn_args, last_warn_kwargs = call
+        self.assertEqual(len(last_warn_args), 1)
+        self.assertEqual(len(last_warn_kwargs), 0)
+        self.assertRegexpMatches(
+            last_warn_args[0],
+            pattern,
+        )
+
+    def test_basic_log_content(self):
+        tsg = timestamps.MonotonicTimestampGenerator()
+        tsg._last_warn = 12
+
+        tsg._next_timestamp(20, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 0)
+        tsg._next_timestamp(16, tsg.last)
+
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+        self.assertLastCallArgRegex(
+            self.patched_timestamp_log.warn.call_args,
+            r'Clock skew detected:.*\b16\b.*\b4\b.*\b20\b'
+        )
+
+    def test_disable_logging(self):
+        no_warn_tsg = timestamps.MonotonicTimestampGenerator(warn_on_drift=False)
+
+        no_warn_tsg.last = 100
+        no_warn_tsg._next_timestamp(99, no_warn_tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 0)
+
+    def test_warning_threshold_respected_no_logging(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_threshold=2,
+        )
+        tsg.last, tsg._last_warn = 100, 97
+        tsg._next_timestamp(98, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 0)
+
+    def test_warning_threshold_respected_logs(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_threshold=1
+        )
+        tsg.last, tsg._last_warn = 100, 97
+        tsg._next_timestamp(98, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+    def test_warning_interval_respected_no_logging(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_interval=2
+        )
+        tsg.last = 100
+        tsg._next_timestamp(70, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+        tsg._next_timestamp(71, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+    def test_warning_interval_respected_logs(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_interval=1
+        )
+        tsg.last = 100
+        tsg._next_timestamp(70, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+        tsg._next_timestamp(72, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 2)

jatin7/python-dse-driver

dse/cluster.py
@@ -68,10 +68,12 @@ from dse.hosts import (Host, _ReconnectionHandler, _HostReconnectionHandler,
 from dse.query import (SimpleStatement, PreparedStatement, BoundStatement,
                        BatchStatement, bind_params, QueryTrace, HostTargetingStatement,
                        named_tuple_factory, dict_factory, tuple_factory, FETCH_SIZE_UNSET)
+from dse.timestamps import MonotonicTimestampGenerator
 
 
 if six.PY3:
     long = int
+
 
 def _is_eventlet_monkey_patched():
     if 'eventlet.patcher' not in sys.modules:
@@ -808,7 +810,8 @@ class Cluster(object):
                  prepare_on_all_hosts=True,
                  reprepare_on_up=True,
                  execution_profiles=None,
-                 allow_beta_protocol_version=False):
+                 allow_beta_protocol_version=False,
+                 timestamp_generator=None):
         """
         ``executor_threads`` defines the number of threads in a pool for handling asynchronous tasks such as
         extablishing connection pools or refreshing metadata.
@@ -854,6 +857,13 @@ class Cluster(object):
 
         if connection_class is not None:
             self.connection_class = connection_class
+
+        if timestamp_generator is not None:
+            if not callable(timestamp_generator):
+                raise ValueError("timestamp_generator must be callable")
+            self.timestamp_generator = timestamp_generator
+        else:
+            self.timestamp_generator = MonotonicTimestampGenerator()
 
         self.profile_manager = ProfileManager()
         profiles = self.profile_manager.profiles
@@ -1693,6 +1703,27 @@ class Session(object):
     .. versionadded:: 2.1.0
     """
 
+    timestamp_generator = None
+    """
+    When :attr:`use_client_timestamp` is set, sessions call this object and use
+    the result as the timestamp.  (Note that timestamps specified within a CQL
+    query will override this timestamp.)  By default, a new
+    :class:`~.MonotonicTimestampGenerator` is created for
+    each :class:`Cluster` instance.
+
+    Applications can set this value for custom timestamp behavior.  For
+    example, an application could share a timestamp generator across
+    :class:`Cluster` objects to guarantee that the application will use unique,
+    increasing timestamps across clusters, or set it to to ``lambda:
+    int(time.time() * 1e6)`` if losing records over clock inconsistencies is
+    acceptable for the application. Custom :attr:`timestamp_generator` s should
+    be callable, and calling them should return an integer representing seconds
+    since some point in time, typically UNIX epoch.
+
+    .. versionadded:: 3.8.0
+    """
+
+
     encoder = None
     """
     A :class:`~dse.encoder.Encoder` instance that will be used when
@@ -1952,8 +1983,8 @@ class Session(object):
             fetch_size = self.default_fetch_size
 
         start_time = time.time()
-        if self.use_client_timestamp:
-            timestamp = int(start_time * 1e6)
+        if self._protocol_version >= 3 and self.use_client_timestamp:
+            timestamp = self.cluster.timestamp_generator()
         else:
             timestamp = None
 

tests/unit/test_timestamps.py
@@ -1,0 +1,168 @@
+# Copyright 2013-2016 DataStax, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest  # noqa
+
+import mock
+
+from dse import timestamps
+
+
+class _TimestampTestMixin(object):
+
+    @mock.patch('dse.timestamps.time')
+    def _call_and_check_results(self,
+                                patched_time_module,
+                                system_time_expected_stamp_pairs,
+                                timestamp_generator=None):
+        """
+        For each element in an iterable of (system_time, expected_timestamp)
+        pairs, call a :class:`dse.timestamps.MonotonicTimestampGenerator`
+        with system_times as the underlying time.time() result, then assert
+        that the result is expected_timestamp. Skips the check if
+        expected_timestamp is None.
+        """
+        patched_time_module.time = mock.Mock()
+        system_times, expected_timestamps = zip(*system_time_expected_stamp_pairs)
+
+        patched_time_module.time.side_effect = system_times
+        tsg = timestamp_generator or timestamps.MonotonicTimestampGenerator()
+
+        for expected in expected_timestamps:
+            actual = tsg()
+            if expected is not None:
+                self.assertEqual(actual, expected)
+
+        # assert we patched timestamps.time.time correctly
+        with self.assertRaises(StopIteration):
+            tsg()
+
+
+class TestTimestampGeneratorOutput(unittest.TestCase, _TimestampTestMixin):
+    """
+    Mock time.time and test the output of MonotonicTimestampGenerator.__call__
+    given different patterns of changing results.
+    """
+
+    def test_timestamps_during_and_after_same_system_time(self):
+        """
+        Timestamps should increase monotonically over repeated system time.
+
+        Test that MonotonicTimestampGenerator's output increases by 1 when the
+        underlying system time is the same, then returns to normal when the
+        system time increases again.
+        """
+        self._call_and_check_results(
+            system_time_expected_stamp_pairs=(
+                (15.0, 15 * 1e6),
+                (15.0, 15 * 1e6 + 1),
+                (15.0, 15 * 1e6 + 2),
+                (15.01, 15.01 * 1e6))
+        )
+
+    def test_timestamps_during_and_after_backwards_system_time(self):
+        """
+        Timestamps should increase monotonically over system time going backwards.
+
+        Test that MonotonicTimestampGenerator's output increases by 1 when the
+        underlying system time goes backward, then returns to normal when the
+        system time increases again.
+        """
+        self._call_and_check_results(
+            system_time_expected_stamp_pairs=(
+                (15.0, 15 * 1e6),
+                (13.0, 15 * 1e6 + 1),
+                (14.0, 15 * 1e6 + 2),
+                (13.5, 15 * 1e6 + 3),
+                (15.01, 15.01 * 1e6))
+        )
+
+
+class TestTimestampGeneratorLogging(unittest.TestCase, _TimestampTestMixin):
+
+    def setUp(self):
+        self.log_patcher = mock.patch('dse.timestamps.log')
+        self.addCleanup(self.log_patcher.stop)
+        self.patched_timestamp_log = self.log_patcher.start()
+
+    def assertLastCallArgRegex(self, call, pattern):
+        last_warn_args, last_warn_kwargs = call
+        self.assertEqual(len(last_warn_args), 1)
+        self.assertEqual(len(last_warn_kwargs), 0)
+        self.assertRegexpMatches(
+            last_warn_args[0],
+            pattern,
+        )
+
+    def test_basic_log_content(self):
+        tsg = timestamps.MonotonicTimestampGenerator()
+        tsg._last_warn = 12
+
+        tsg._next_timestamp(20, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 0)
+        tsg._next_timestamp(16, tsg.last)
+
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+        self.assertLastCallArgRegex(
+            self.patched_timestamp_log.warn.call_args,
+            r'Clock skew detected:.*\b16\b.*\b4\b.*\b20\b'
+        )
+
+    def test_disable_logging(self):
+        no_warn_tsg = timestamps.MonotonicTimestampGenerator(warn_on_drift=False)
+
+        no_warn_tsg.last = 100
+        no_warn_tsg._next_timestamp(99, no_warn_tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 0)
+
+    def test_warning_threshold_respected_no_logging(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_threshold=2,
+        )
+        tsg.last, tsg._last_warn = 100, 97
+        tsg._next_timestamp(98, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 0)
+
+    def test_warning_threshold_respected_logs(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_threshold=1
+        )
+        tsg.last, tsg._last_warn = 100, 97
+        tsg._next_timestamp(98, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+    def test_warning_interval_respected_no_logging(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_interval=2
+        )
+        tsg.last = 100
+        tsg._next_timestamp(70, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+        tsg._next_timestamp(71, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+    def test_warning_interval_respected_logs(self):
+        tsg = timestamps.MonotonicTimestampGenerator(
+            warning_interval=1
+        )
+        tsg.last = 100
+        tsg._next_timestamp(70, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 1)
+
+        tsg._next_timestamp(72, tsg.last)
+        self.assertEqual(len(self.patched_timestamp_log.warn.call_args_list), 2)