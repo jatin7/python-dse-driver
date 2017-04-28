2b02d26a65fb8d62049544d0f7b22121038d8b14
Jim Witschey <jim.witschey@gmail.com>
2017-04-28 10:57:21 -0400

    s/Apache License/DataStax DSE Driver License/g

--- examples/request_init_listener.py
+++ examples/request_init_listener.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # Copyright 2013-2017 DataStax, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the DataStax DSE Driver License;
 # you may not use this file except in compliance with the License.
+#
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# http://www.datastax.com/terms/datastax-dse-driver-license-terms
 
 # This script shows an example "request init listener" which can be registered to track certain request metrics
 # for a session. In this case we're just accumulating total request and error counts, as well as some statistics
