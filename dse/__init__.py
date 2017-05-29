7b769212dd3ab86388f62dc82896084934da38f4
Alan Boudreault <alan@alanb.ca>
2017-05-29 15:55:22 -0400

    post version

--- dse/__init__.py
+++ dse/__init__.py
@@ -17,7 +17,7 @@ class NullHandler(logging.Handler):
 
 logging.getLogger('dse').addHandler(NullHandler())
 
-__version_info__ = (1, 1, 1)
+__version_info__ = (1, 1, 1, 'post0')
 __version__ = '.'.join(map(str, __version_info__))
 
 _core_driver_target_version = '3.10'
